882e89c9ca06b760ec740d27d01cd4291192cefa
AhmedElmasry700 <ahmedsalah.eg70@gmail.com>
2019-02-24 01:46:27 +0200

    widget + detail activity + admob + crash reporting

---
+++ app/src/main/java/com/example/salah/ahmed/newsapp/Widget/NewAppWidget.java
@@ -0,0 +1,52 @@
+package com.example.salah.ahmed.newsapp.Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.RemoteViews;
+
+import com.example.salah.ahmed.newsapp.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+    private static final String KEY_TITLE_PREFERENCES = "widget_title";
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+//        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
+        SharedPreferences pre = context.getSharedPreferences(KEY_TITLE_PREFERENCES, 0);
+
+        String NeWs = pre.getString("news_title_description", "no data");
+
+        views.setTextViewText(R.id.title_text, NeWs);
+
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
